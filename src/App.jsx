57455c2dbbabe9feaeb16d9625e0cbb4800708b0
Miriyan Asenov <mirorim.789@gmail.com>
2025-03-30 20:51:44 +0300

    3. Modified file: App.jsx; new file - Input.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 import Info from './components/Info';
 import Button from './components/Button';
+import Input from './components/Input';
 
 function App(){
   const [secNum, setSecNum] = useState( Math.trunc( Math.random() * 100 ) + 1 );
-  const secNumInfo = secNum + ' is the secret number';
+  const inp = useRef('');
+  const [guess, setGuess] = useState(null);
+  const secNumInfo = guess === null ? 'Guess the number' : guess === secNum ? 'Yes' : 'No - ' + secNum;
+
+  function handleCheck(){
+    setGuess( Number(inp.current.value) );
+  }
 
   function handleNewGame(){
     setSecNum( Math.trunc( Math.random() * 100 ) + 1 );
+    inp.current.value = '';
+    setGuess(null);
   }
 
   return (
     <>
       <Info message={secNumInfo} />
+      <Input inpRef={inp} />
+      <Button handleOnClick={handleCheck}>Check</Button>
       <Button handleOnClick={handleNewGame}>New Game</Button>
     </>
   );
---
+++ src/components/Input.jsx
@@ -0,0 +1,5 @@
+function Input({inpRef}){
+    return <input type='number' ref={inpRef} />;
+}
+
+export default Input;
