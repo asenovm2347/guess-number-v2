f4c0e99e078429dc56a5c0cbb88993c009b54b25
asenovm2347 <asenovm2347@gmail.com>
2025-03-30 20:55:44 +0300

    4. Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,47 @@ import Info from './components/Info';
 import Button from './components/Button';
 import Input from './components/Input';
 
+function secNumInfo(sN, gN, aN){
+  let message;
+  if(gN === null){
+    message = 'Guess the number';
+  }else if(gN === sN){
+    message = 'Yes - ' + sN;
+  }else if(aN > 0){
+    if(gN < sN){
+      message = 'Too small. Attempts left - ' + aN;
+    }else{
+      message = 'Too big. Attempts left - ' + aN;
+    }
+  }else{
+    message = 'No - ' + sN;
+  } 
+  return message;
+}
+
 function App(){
   const [secNum, setSecNum] = useState( Math.trunc( Math.random() * 100 ) + 1 );
   const inp = useRef('');
   const [guess, setGuess] = useState(null);
-  const secNumInfo = guess === null ? 'Guess the number' : guess === secNum ? 'Yes' : 'No - ' + secNum;
+  const [attempts, setAttempts] = useState(5);
+  const info = secNumInfo(secNum, guess, attempts);
 
   function handleCheck(){
     setGuess( Number(inp.current.value) );
+    setAttempts(prev => --prev);
+    inp.current.value = '';
   }
 
   function handleNewGame(){
     setSecNum( Math.trunc( Math.random() * 100 ) + 1 );
     inp.current.value = '';
+    setAttempts(5);
     setGuess(null);
   }
 
   return (
     <>
-      <Info message={secNumInfo} />
+      <Info message={info} />
       <Input inpRef={inp} />
       <Button handleOnClick={handleCheck}>Check</Button>
       <Button handleOnClick={handleNewGame}>New Game</Button>
